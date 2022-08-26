7aca7b26aeb306002843810c9d1b664f7d519d80
Simon Wong <simonwong1985hk@gmail.com>
2022-08-26 09:39:49 +0800

    update profile

--- src/components/content/Profile.vue
+++ src/components/content/Profile.vue
@@ -2,18 +2,27 @@
   <div id="profile" class="prose">
     <h2 class="section-headline">Profile</h2>
     <p>
-      I'm a <span class="font-bold">self-taught</span> Full Stack Web Developer focusing on the core web
-      technologies: <span class="font-bold">HTML, CSS, JavaScript, SQL and PHP</span>.</p>
+      My name is Simon Wong, and I'm a {{ new Date().getFullYear() - 2015 }} year old developer living in the
+      Hong Kong.
+    </p>
     <p>
-      I'm experienced in frontend framework like <span class="font-bold">Vue.js</span>, backend framework like <span
-        class="font-bold">Laravel</span> and
-      CMS like <span class="font-bold">WordPress</span>, <span class="font-bold">Magento</span>
+      Specifically, I'm a <span class="font-bold">self-taught</span> Full Stack Web
+      Developer focusing on the core web technologies:
+      <span class="font-bold">HTML, CSS, JavaScript, SQL and PHP</span>.
+    </p>
+    <p>
+      I'm experienced in frontend framework like
+      <span class="font-bold">Vue.js</span>, backend framework like
+      <span class="font-bold">Laravel</span> and CMS like
+      <span class="font-bold">WordPress</span>,
+      <span class="font-bold">Magento</span>
       to build a robust web app.
     </p>
     <p>
-      I'm familiar with using CSS frameworks like <span class="font-bold">TailwindCSS</span>, <span
-        class="font-bold">Bootstrap</span> to
-      speed up styling of my projects.
+      I'm familiar with using CSS frameworks like
+      <span class="font-bold">TailwindCSS</span>,
+      <span class="font-bold">Bootstrap</span> to speed up styling of my
+      projects.
     </p>
   </div>
   <hr class="mt-8 mb-12" />
